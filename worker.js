25899cb314b21ea76936e7b7e538bd2b958e9386
caffeines <sadat.talks@gmail.com>
2020-02-10 16:34:27 +0600

    work queues done #2

--- worker.js
+++ worker.js
@@ -11,14 +11,17 @@ amqp.connect('amqp://localhost', (err, con) => {
     if (err) {
       throw err;
     } 
-
-    const queue = 'hello';
-
-    channel.assertQueue(queue, { durable: false });
+    const queue = 'task_queue';
+    channel.assertQueue(queue, { durable: true });
+    channel.prefetch(1);
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
     channel.consume(queue, msg => {
+      const secs = msg.content.toString().split('.').length - 1;
       console.log(" [x] Received %s", msg.content.toString());
-    }, {noAck: true });
+      setTimeout(function() {
+        console.log(" [x] Done");
+      }, secs * 1000);
+    }, {noAck: false });
   });
 });
